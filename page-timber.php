5e6f419e76c24ef9ca02a68837277c9b55733a35
Jared Novack <jared@upstatement.com>
2014-07-28 10:47:11 -0400

    added files for Timber "built by" section

---
+++ page-timber.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the wordpress construct of pages
+ * and that other 'pages' on your wordpress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Boilerplate
+ * @since Boilerplate 1.0
+ */
+
+
+	$data = Timber::get_context();
+	$pi = new TimberPost();
+	$data['post'] = $pi;
+	$data['body_class'] .= ' page-'.$pi->post_name;
+	$template_file = 'page.twig';
+	if (file_exists($_SERVER['DOCUMENT_ROOT'].$data['theme_dir'].'/css/'.$pi->post_type.'-'.$post->post_name.'.css')){
+		$data['post']->css_file = $data['theme_dir'].'/css/'.$pi->post_type.'-'.$post->post_name.'.css';
+	}
+	require_once(__DIR__ . '/vendor/tan-tan-kanarek/github-php-client/tan-tan-kanarek/github-php-client/client/GitHubClient.php');
+
+	$client = new GitHubClient();
+	$auth = file_get_contents(__DIR__.'/auth.json');
+	$auth = json_decode($auth);
+	$client->setCredentials($auth->github->user, $auth->github->pass);
+	$contribs = $client->repos->listContributors('jarednova', 'timber');
+	$data['contribs'] = $contribs;
+	Timber::render(array('custom/page-'.$pi->post_name.'.twig', 'page-'.$pi->post_name.'.twig', 'page.twig'), $data);
