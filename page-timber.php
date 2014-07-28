f1eecb60e553cd670fbcd85e1364406999d6aa31
Jared Novack <jared@upstatement.com>
2014-07-28 10:55:31 -0400

    made contribs a transient

--- page-timber.php
+++ page-timber.php
@@ -13,20 +13,29 @@
  */
 
 
-	$data = Timber::get_context();
-	$pi = new TimberPost();
-	$data['post'] = $pi;
-	$data['body_class'] .= ' page-'.$pi->post_name;
-	$template_file = 'page.twig';
-	if (file_exists($_SERVER['DOCUMENT_ROOT'].$data['theme_dir'].'/css/'.$pi->post_type.'-'.$post->post_name.'.css')){
-		$data['post']->css_file = $data['theme_dir'].'/css/'.$pi->post_type.'-'.$post->post_name.'.css';
-	}
-	require_once(__DIR__ . '/vendor/tan-tan-kanarek/github-php-client/tan-tan-kanarek/github-php-client/client/GitHubClient.php');
-
-	$client = new GitHubClient();
-	$auth = file_get_contents(__DIR__.'/auth.json');
-	$auth = json_decode($auth);
-	$client->setCredentials($auth->github->user, $auth->github->pass);
-	$contribs = $client->repos->listContributors('jarednova', 'timber');
-	$data['contribs'] = $contribs;
-	Timber::render(array('custom/page-'.$pi->post_name.'.twig', 'page-'.$pi->post_name.'.twig', 'page.twig'), $data);
+$data = Timber::get_context();
+$pi = new TimberPost();
+$data['post'] = $pi;
+$data['body_class'] .= ' page-'.$pi->post_name;
+$template_file = 'page.twig';
+if ( file_exists( $_SERVER['DOCUMENT_ROOT'].$data['theme_dir'].'/css/'.$pi->post_type.'-'.$post->post_name.'.css' ) ) {
+	$data['post']->css_file = $data['theme_dir'].'/css/'.$pi->post_type.'-'.$post->post_name.'.css';
+}
+$contribs = TimberHelper::transient( 'aatimbaaer-contribs', function() {
+		require_once __DIR__ . '/vendor/tan-tan-kanarek/github-php-client/tan-tan-kanarek/github-php-client/client/GitHubClient.php';
+		$client = new GitHubClient();
+		$auth = file_get_contents( __DIR__.'/auth.json' );
+		$auth = json_decode( $auth );
+		$client->setCredentials( $auth->github->user, $auth->github->pass );
+		$contribs = $client->repos->listContributors( 'jarednova', 'timber' );
+		$ret = array();
+		foreach ( $contribs as $contrib ) {
+			$obj = new stdClass();
+			$obj->login = $contrib->getLogin();
+			$obj->avatar_url = $contrib->getAvatarUrl();
+			$ret[] = $obj;
+		}
+		return $ret;
+	}, 1200 );
+$data['contribs'] = $contribs;
+Timber::render( array( 'custom/page-'.$pi->post_name.'.twig', 'page-'.$pi->post_name.'.twig', 'page.twig' ), $data );
