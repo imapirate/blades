2fc3b2e3b3a108b7bcf9db968f81f945c95d3d46
Jared Novack <jarednova@upstatement.com>
2014-09-10 22:30:08 -0400

    Update template-person.php

--- template-person.php
+++ template-person.php
@@ -3,16 +3,13 @@
 Template Name: Person
 */
 /**
- * The template for displaying all pages.
+ * The template for displaying people pages.
  *
  * This is the template that displays all pages by default.
  * Please note that this is the wordpress construct of pages
  * and that other 'pages' on your wordpress site will use a
  * different template.
  *
- * @package WordPress
- * @subpackage Boilerplate
- * @since Boilerplate 1.0
  */
 
 	$data = Timber::get_context();
