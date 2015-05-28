6faf2d521d565855dc9a86181e3674a741768903
Jared Novack <jared@upstatement.com>
2015-05-28 16:33:57 -0400

    Added PW protection

--- single-project.php
+++ single-project.php
@@ -1,8 +1,11 @@
 <?php
 
-	$post = new PH_Project();
-	$data['post'] = $post;
-	$data['user'] = new TimberUser();
-	$data['theme'] = new TimberTheme();
-	Timber::render('/pm-home/single-project.twig', $data);
-
+$post = new PH_Project();
+$data['post'] = $post;
+$data['user'] = new TimberUser();
+$data['theme'] = new TimberTheme();
+if ( post_password_required( $post->ID ) ) {
+	Timber::render( '/pm-home/single-project-password.twig', $data );
+} else {
+	Timber::render( '/pm-home/single-project.twig', $data );
+}
