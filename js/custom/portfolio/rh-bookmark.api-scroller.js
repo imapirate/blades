8943108c1fcb19143fb32c61a316659a7d9007ea
Emma Baker <emmabaker@Emmas-MacBook-Pro-2.local>
2015-09-10 14:37:51 -0400

    Reviewed JS in rh-bookmark.api-scroller.js

--- js/custom/portfolio/rh-bookmark.api-scroller.js
+++ js/custom/portfolio/rh-bookmark.api-scroller.js
@@ -20,13 +20,13 @@
   pathPrepare($down);
 
 
-  var $knotOffset = $('#api-p-intro').offset().top;
+  var knotOffset = $('#api-p-intro').offset().top;
   
   $(document).scroll(function() {
   
-    var $scrollHeight = $(window).scrollTop();
+    var scrollHeight = $(window).scrollTop();
 
-    if ($scrollHeight >= $knotOffset) {
+    if (scrollHeight >= knotOffset) {
       $knot.css({
         'stroke-dashoffset': 0
       })
@@ -35,47 +35,32 @@
 
   });
 
-
-
   // init controller
   var controller = new ScrollMagic.Controller();
 
   // build tween
-
-  // var tween0 = new TimelineMax()
-  //   .add(TweenMax.to($knot, 0.9, {strokeDashoffset: 0, ease:Linear.easeNone})) // draw word for 0.9
-    
-  // // build scene
-  // var scene = new ScrollMagic.Scene({triggerElement: "#knot", duration: 400, tweenChanges: true})
-  //         .setTween(tween0)
-  //         .addIndicators() // add indicators (requires plugin)
-  //         .addTo(controller);
-
-  var tween1 = new TimelineMax()
+  var page_tween = new TimelineMax()
     .add(TweenMax.to($page, 0.9, {strokeDashoffset: 0, ease:Linear.easeNone})) // draw word for 0.9
     
   // build scene
-  var scene = new ScrollMagic.Scene({triggerElement: "#page-lines", duration: 400, tweenChanges: true})
-          .setTween(tween1)
-          // .addIndicators() // add indicators (requires plugin)
+  new ScrollMagic.Scene({triggerElement: "#page-lines", duration: 400, tweenChanges: true})
+          .setTween(page_tween)
           .addTo(controller);
 
-  var tween2 = new TimelineMax()
+  var book_tween = new TimelineMax()
     .add(TweenMax.to($book, 0.9, {strokeDashoffset: 0, ease:Linear.easeNone})) // draw word for 0.9
     
   // build scene
-  var scene = new ScrollMagic.Scene({triggerElement: "#api-p-one", duration: 400, tweenChanges: true})
-          .setTween(tween2)
-          // .addIndicators() // add indicators (requires plugin)
+  new ScrollMagic.Scene({triggerElement: "#api-p-one", duration: 400, tweenChanges: true})
+          .setTween(book_tween)
           .addTo(controller);
 
-  var tween3 = new TimelineMax()
+  var down_tween = new TimelineMax()
     .add(TweenMax.to($down, 0.9, {strokeDashoffset: 0, ease:Linear.easeNone})) // draw word for 0.9
     
   // build scene
-  var scene = new ScrollMagic.Scene({triggerElement: "#downward-lines", duration: 400, tweenChanges: true})
-          .setTween(tween3)
-          // .addIndicators() // add indicators (requires plugin)
+  new ScrollMagic.Scene({triggerElement: "#downward-lines", duration: 400, tweenChanges: true})
+          .setTween(down_tween)
           .addTo(controller);
 
 })(jQuery);
